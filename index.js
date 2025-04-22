5d6fae46d2eb9948b05a5a52c6aedee6ec63e76b
Arjunlama2 <instructorarjun1@gmail.com>
2025-04-22 16:38:44 +0545

    todo idea

--- index.js
+++ index.js
@@ -5,3 +5,46 @@ console.log("Hello world")
 // git add .
 // git commit -m "message here"
 // git push
+
+
+const tasks=[
+   
+]
+
+
+function addTask(event){
+    event.preventDefault()
+   
+ let task=event.target.task.value
+
+ 
+ tasks.push(task)
+ console.log(tasks)
+
+ displayTask()
+
+}
+
+
+
+
+function displayTask(){
+const container=document.getElementById("container")
+let string=""
+ const stringTask=tasks.reverse().forEach((el)=>{
+ string+=`<li>${el} <button  onClick="deleteTask()">Delete</button></li>`
+
+})
+
+container.innerHTML=string
+
+}
+
+
+
+
+function deleteTask(index){
+    console.log("Delete")
+
+    console.log(index)
+}
